1a47a2db446e4e05106a84cbad0e56ba4ff225de
timofoy2000 <timo-tim@bk.ru>
2024-03-21 10:33:52 +0300

    checker for button

--- script/checker.js
+++ script/checker.js
@@ -1,12 +1,15 @@
 var passwordInput = document.getElementById("passreg");
 var passwordError = document.getElementById("passwordError");
-
+var registerButton = document.getElementById("regr");
 
 passwordInput.addEventListener("input", function() {
     var password = passwordInput.value;
     if (password.length < 8) {
         passwordError.style.display = "block";
+        registerButton.setAttribute("disabled", "disabled"); 
+
     } else {
         passwordError.style.display = "none";
+        registerButton.removeAttribute("disabled"); 
     }
 });
