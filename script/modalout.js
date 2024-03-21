baf80b04bcad6012fbb65025a2a9281b2564d0e9
timofoy2000 <timo-tim@bk.ru>
2024-03-21 22:57:17 +0300

    оогнпеа

---
+++ script/modalout.js
@@ -0,0 +1,29 @@
+let user = document.getElementsByClassName('sign')[0];
+let userTable = document.getElementsByClassName('userTable')[0];
+let tel = "+79257772244";
+
+user.onclick = function(event) {
+    event.stopPropagation(); // Предотвращаем всплытие события клика
+    if (userTable.style.display === 'flex') {
+        userTable.style.display = 'none'; // Закрываем userTable, если он уже открыт
+    } else {
+        userTable.style.display = 'flex'; // Иначе открываем userTable
+    }
+};
+
+window.onclick = function(event) {
+    if (event.target !== userTable && userTable.style.display === 'flex') {
+        userTable.style.display = 'none';
+    }
+};
+
+// Добавляем обработчик клика для userTable
+userTable.onclick = function(event) {
+    event.stopPropagation(); // Предотвращаем всплытие события клика
+};
+
+
+
+//  теперь само модальное окно
+
+document.getElementsByClassName('tel')[0].textContent = tel
